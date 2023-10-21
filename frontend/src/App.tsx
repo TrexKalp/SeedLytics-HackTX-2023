dc4202841598820042a6211d40794003d1334d1b
NikhilVeeram <80705702+NikhilVeeram@users.noreply.github.com>
2023-10-21 13:07:13 -0500

    changed to simple message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-// src/App.tsx
-import React from "react";
-import Predictor from "./components/Predictor";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Make an HTTP request to the Flask API
+    axios.get("/api/hello")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Predictor />
+      
+      {message && <p>{message}</p>}
     </div>
   );
 };
